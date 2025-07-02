6a675d1da826f27d8048ee823fcf348f94e7339c
PeterMescher <peter@mescher.net>
2025-07-02 08:43:53 -0400

    More syntax cleanup (up to 23 resources in the plan!)

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,7 +1,7 @@
 resource "aws_dynamodb_table" "crc_resume_counter_table" {
   name           = "crc_resume_counter"
   billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "id"
+  hash_key       = "page_name"
   attribute {
     name = "page_name"
     type = "S"
