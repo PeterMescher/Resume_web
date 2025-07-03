a2489124c3ebda47c543b1feee7c8eb87f6af7e0
PeterMescher <peter@mescher.net>
2025-07-03 18:06:38 -0400

    *sigh* That was a valuable lesson in how *not* to use ChatGPT to debug. I made a minor error when creating the Route53 Alias for the API Gateway. (I failed to reference the domain_name_configuration underneath the domain_name resource) I used ChatGPT to look at the error message, and it correctly gave me the correct code, straight out of the TF AWS provider docs. I thought that is the code I was using, so I told ChatGPT that the hosted_zone wasn't exported... Instead of sending me a link to the docs showing me that I was wrong, it agreed with me, and sent me down a rabbit-hole of a god-awful shell script (that it helpfully wrote, badly, requiring a bunch of debugging)) to extract the necessary info from the CLI output.

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -66,33 +66,14 @@ resource "aws_route53_record" "www_site_hosting_AAAA_record" {
   }
 }
 
-# Gosh durn it, apigatewayv2 does not expose the hosted zone ID for the endpoint, so I have to
-# extract it from the CLI Command for the API Gateway V2 domain name command.
-# (I could build a table of the hosted zone IDs by region manually, but I'd hate to get caught off-guard if they change.)
-
-# Using the CLI for this is explictly called for by the AWS API docs, as of Jul 2025:
-# https://docs.aws.amazon.com/Route53/latest/APIReference/API_AliasTarget.html
-
-data "external" "website_counter_apigwv2_domain_info" {
-  program = ["bash", "${path.module}/get_apigw_domain.sh"]
-  query = {
-    domain_name = aws_apigatewayv2_domain_name.crc_website_counter_domain.domain_name
-    region      = var.aws_region
-    aws_profile = var.aws_profile
-  }
-
-  depends_on = [ aws_apigatewayv2_domain_name.crc_website_counter_domain ]
-}
-
-
 resource "aws_route53_record" "crc_website_counter_api_A_record" {
   zone_id = aws_route53_zone.crc_resume_zone.zone_id
   name    = "api.${var.site_domain_prefix}.${var.site_base_domain}"
   type    = "A"
 
   alias {
-    name                   = data.external.website_counter_apigwv2_domain_info.result["target_domain_name"]
-    zone_id                = data.external.website_counter_apigwv2_domain_info.result["hosted_zone_id"]
+    name                   = aws_apigatewayv2_domain_name.crc_website_counter_domain.domain_name_configuration[0].target_domain_name
+    zone_id                = aws_apigatewayv2_domain_name.crc_website_counter_domain.domain_name_configuration[0].hosted_zone_id
     evaluate_target_health = true
   }
   
@@ -104,9 +85,9 @@ resource "aws_route53_record" "crc_website_counter_api_AAAA_record" {
   type    = "AAAA"
 
   alias {
-    name                   = data.external.website_counter_apigwv2_domain_info.result["target_domain_name"]
-    zone_id                = data.external.website_counter_apigwv2_domain_info.result["hosted_zone_id"]
-    evaluate_target_health = true
+    name                   = aws_apigatewayv2_domain_name.crc_website_counter_domain.domain_name_configuration[0].target_domain_name
+    zone_id                = aws_apigatewayv2_domain_name.crc_website_counter_domain.domain_name_configuration[0].hosted_zone_id
+     evaluate_target_health = true
   }
   
 }
